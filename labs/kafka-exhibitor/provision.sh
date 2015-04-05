6d898e20af30c887e3d11132708a9c933e48118a
James Oliver <james.oliver@stealth.ly>
2015-04-05 16:07:56 -0700

    Cleanup packer definition

--- labs/kafka-exhibitor/provision.sh
+++ labs/kafka-exhibitor/provision.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -Eu
 
+kafka_version=0.8.2.1
+
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 apt-get update
@@ -17,3 +19,9 @@ mkdir -p /opt/exhibitor
 cd /opt/exhibitor
 curl -O https://raw.githubusercontent.com/Netflix/exhibitor/master/exhibitor-standalone/src/main/resources/buildscripts/standalone/gradle/build.gradle
 gradle shadowJar
+
+# Install latest version of Kafka
+cd /opt
+curl -O https://dist.apache.org/repos/dist/release/kafka/$kafka_version/kafka_2.11-$kafka_version.tgz
+tar -xzf kafka_2.11-$kafka_version.tgz && rm kafka_2.11-$kafka_version.tgz
+ln -s kafka_2.11-0.8.2.1 kafka
